fa7798711a85a853dc2e1c3e0b866b20f9dd33fa
Aanad Kumar Satapathy <anandkumarsatapathy@gmail.com>
2022-12-29 21:38:46 +0530

    working-on-Form-page

---
+++ TecqN-client/src/screens/loginPage/Form.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import EditOutlined from "@mui/icons-material/EditOutlined";
+import { Formik } from "formik";
+import * as yup from "yup";
+
+const Form = () => {
+  return <div>Form</div>;
+};
+
+export default Form;
