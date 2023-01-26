8101459cd8c3de523526008eb7fbd18b15f8e023
Aanad Kumar Satapathy <anandkumarsatapathy@gmail.com>
2023-01-26 21:34:22 +0530

    working-on-friend-components

---
+++ TecqN-client/src/components/Friend.jsx
@@ -0,0 +1,13 @@
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setFriends } from "state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+
+const Friend = () => {
+  return <div>Friend</div>;
+};
+
+export default Friend;
