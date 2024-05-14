bc874711abfaef8539b1e1e6082fddf3920f5fee
Chandransu <cse.22bcsd19@silicon.ac.in>
2024-05-14 21:27:28 +0530

    ok

--- TecHN-backend/index.js
+++ TecHN-backend/index.js
@@ -3,9 +3,9 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import multer from "multer";
+import multer from "multer"; //
 import helmet from "helmet";
-import morgan from "morgan";
+import morgan from "morgan"; //
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
